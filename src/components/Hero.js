936b973ed682546265d58039a209632aa8ec9c44
sunny-github-acc <54981869+sunny-github-acc@users.noreply.github.com>
2021-03-18 21:49:07 +0200

    Updated conversation logic

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,46 +1,54 @@
-import React, { useCallback, useEffect, useState } from "react"
+import React, { useEffect, useState } from "react"
+import HeroElements from "./HeroElements"
 import { handleBtnContact, handleBtnProjects } from "./Navbar"
 
 const Hero = () => {
-  const [state, setState] = useState()
-  const [talk, setTalk] = useState([
-    <h1 className="bubble -right">
-      Hi! My name is <strong>Karolis Kaz</strong>.
-    </h1>,
-    <h3 className="bubble -right">
-      I enjoy{" "}
-      <button onClick={handleBtnProjects}>
-        <strong>developing software </strong>
-      </button>
-      using latest technologies.
-    </h3>,
-    <h3 className="bubble -right">
-      I'm based in Lithuania, putting the most emphasis in building websites.{" "}
-    </h3>,
-    <h3 className="bubble -right">
-      Head <button>this way</button> for more details.
-    </h3>,
-    <h3 className="bubble -right">
-      Why don't you <button onClick={handleBtnContact}>tell me</button> more
-      about yourself?
-    </h3>,
+  const [texts, setTexts] = useState()
+  const [allTexts, setAllTexts] = useState([
+    [
+      <>Hello there!</>,
+      <>
+        I'm <span className="strong">Karolis Kaz</span> and this is my
+        portfolio.
+      </>,
+      <> How may I call you?</>,
+    ],
+    [
+      <>
+        I enjoy{" "}
+        <button onClick={handleBtnProjects}>
+          <strong>developing software </strong>
+        </button>
+        using latest technologies, putting the most emphasis in building
+        websites.
+      </>,
+    ],
+    [<>Head this way for more details.</>],
+    [
+      <>
+        Why don't you <button onClick={handleBtnContact}>tell me</button> more
+        about yourself?
+      </>,
+    ],
   ])
 
-  const handleState = useCallback(() => {
-    setState(talk[0])
-    setTalk(talk.slice(1))
-    console.log(talk)
-  }, [])
+  const handleClick = () => {
+    if (allTexts.length) {
+      setTexts(prev => (prev ? [...prev, allTexts[0]] : [allTexts[0]]))
+      setAllTexts(prev => prev.shift())
+    }
+  }
 
   useEffect(() => {
-    handleState()
+    handleClick()
   }, [])
 
-  const handleClick = () => {}
   return (
     <section className="hero ">
-      <div className="small-talk">{state}</div>
-      <button onClick={handleClick}>:)</button>
+      <div className="small-talk">
+        <HeroElements texts={texts} />
+        <button onClick={handleClick}>:)</button>
+      </div>
     </section>
   )
 }
