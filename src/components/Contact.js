f0cf115811ef9250e13a28594848288ab51a5985
sunny-github-acc <karolis.kazak@gmail.com>
2021-04-09 14:33:04 +0300

    Added formspree

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,21 @@
 import React from "react"
+import { useForm, ValidationError } from "@formspree/react"
 
 const Contact = ({ visitorsAnswers, handleName, handleEmail }) => {
+  const [state, handleSubmit] = useForm("xzbybyjo")
+
+  if (state.succeeded) {
+    return <p>Thanks for joining!</p>
+  }
+
   return (
     <section className="hero">
-      <form className="contact pixelated">
+      <form onSubmit={handleSubmit} className="contact pixelated" method="POST">
         <div className="input-container">
           <h1 className="h1">Your name:</h1>
-          <label className="label name">
+          <label htmlFor="name" className="label name">
             <input
+              id="name"
               className="input"
               type="text"
               name="name"
@@ -15,12 +23,14 @@ const Contact = ({ visitorsAnswers, handleName, handleEmail }) => {
               onChange={handleName}
               required
             />
+            <ValidationError prefix="Name" field="name" errors={state.errors} />
           </label>
         </div>
         <div className="input-container">
           <h1 className="h1">Your email:</h1>
-          <label className="label email">
+          <label className="label email" htmlFor="email">
             <input
+              id="email"
               className="input"
               type="text"
               name="email"
@@ -28,29 +38,46 @@ const Contact = ({ visitorsAnswers, handleName, handleEmail }) => {
               onChange={handleEmail}
               required
             />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
           </label>
         </div>
         <div className="input-container">
           <h1 className="h1">Field of interests:</h1>
-          <label className="label subject">
+          <label className="label subject" htmlFor="subject">
             <input
+              id="subject"
               className="input"
               type="text"
               name="subject"
               placeholder={visitorsAnswers.subject}
               required
             />
+            <ValidationError
+              prefix="Subject"
+              field="subject"
+              errors={state.errors}
+            />
           </label>
         </div>
         <div className="input-container">
           <h1 className="h1">Mission:</h1>
-          <label className="label message">
+          <label className="label message" htmlFor="message">
             <textarea
+              id="message"
               className="textarea"
               type="textarea"
               name="message"
               placeholder={visitorsAnswers.message}
               required
+            />
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
             />
           </label>
         </div>
